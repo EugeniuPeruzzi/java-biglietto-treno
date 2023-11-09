1ba29c8ae6410acf461896f88a08d496b1fc408d
EugeniuPeruzzi <jaka1997@live.it>
2023-11-09 17:33:56 +0100

    snack 2 done

--- src/org/lessons/java/CalcolaBiglietto.java
+++ src/org/lessons/java/CalcolaBiglietto.java
@@ -10,7 +10,8 @@ public class CalcolaBiglietto {
 		
 		// Standard price 
 		final double PRICE_KM = 0.21;
-		
+		int underAge = 18;
+		int over65 = 65;
 		//Kilometers to do 
 		System.out.print("Inserisci la distanza da percorre:");
 		int distance = Integer.valueOf(in.nextLine());
@@ -19,10 +20,20 @@ public class CalcolaBiglietto {
 		System.out.print("Inserisci la tua eta:");
 		int userAge = Integer.valueOf(in.nextLine());
 		
-		// standard price
+		//standard price
 		double standardPrice = distance * PRICE_KM;
+		double discount = 0;
+		//discount condition
+		if (userAge < underAge) {
+			discount = standardPrice * 0.20;
+		}
+		else if (userAge > over65){
+			discount = standardPrice * 0.40;
+		}
 		
+		double discountedPrice = standardPrice - discount;
 		
+		System.out.println((userAge < underAge) ? discountedPrice : ((userAge > over65) ? discountedPrice : standardPrice));
 		
 		
 		// stop scanner
