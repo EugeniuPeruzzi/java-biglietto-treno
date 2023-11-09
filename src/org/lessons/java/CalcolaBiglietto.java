720be015fbc1f08945fbe75617e2aa949b1a23cb
EugeniuPeruzzi <jaka1997@live.it>
2023-11-09 17:21:44 +0100

    added user inputs

--- src/org/lessons/java/CalcolaBiglietto.java
+++ src/org/lessons/java/CalcolaBiglietto.java
@@ -1,10 +1,32 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class CalcolaBiglietto {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// call out scanner
+		Scanner in = new Scanner(System.in);
+		
+		// Standard price 
+		final double PRICE_KM = 0.21;
+		
+		//Kilometers to do 
+		System.out.print("Inserisci la distanza da percorre:");
+		int distance = Integer.valueOf(in.nextLine());
+		
+		//User age
+		System.out.print("Inserisci la tua eta:");
+		int userAge = Integer.valueOf(in.nextLine());
+		
+		// standard price
+		double standardPrice = distance * PRICE_KM;
+		
+		
+		
+		
+		// stop scanner
+		in.close();
 	}
 
 }
